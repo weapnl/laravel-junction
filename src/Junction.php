56fe5c02c2b8659b82b57bd47314a89a3591516d
jobjen02 <jobjen02@gmail.com>
2024-03-07 14:24:45 +0100

    Update Junction.php

--- src/Junction.php
+++ src/Junction.php
@@ -11,11 +11,27 @@ class Junction
      * @param $controller
      * @return void
      */
-    public static function resource($uri, $controller): void
+    public static function resource($uri, $controller, $only = ['index', 'show', 'store', 'update', 'destroy', 'action']): void
     {
-        Route::apiResource($uri, $controller);
-        Route::post($uri . '/index', $controller. '@index');
-        Route::post($uri . '/{id}/show', $controller. '@show');
-        Route::put($uri, [$controller, 'action']);
+        Route::apiResource($uri, $controller)->only(array_diff($only, ['action']));
+
+        if (in_array('index', $only)) {
+            Route::post($uri . '/index', $controller. '@index');
+        }
+
+        if (in_array('show', $only)) {
+            Route::post($uri . '/{id}/show', $controller. '@show');
+        }
+
+        if (in_array('action', $only)) {
+            Route::put($uri, $controller . '@action');
+        }
+    }
+
+    public function only($methods)
+    {
+        $this->options['only'] = is_array($methods) ? $methods : func_get_args();
+
+        return $this;
     }
 }
