cfa17ecb718bfce4e354747f37aa6f677dded168
Job <jobjen02@gmail.com>
2025-07-15 16:43:14 +0200

    Comment update

--- config/junction.php
+++ config/junction.php
@@ -36,8 +36,8 @@ return [
     ],
 
     /*
-     * Wraps store, update, destroy and media upload logic
-     * in a database transaction. If enabled, all DB operations
+     * Wraps store, update, destroy and action logic
+     * in a database transaction. If enabled, the DB operations
      * in those methods will be committed only if successful,
      * otherwise rolled back on failure. Ensures consistency
      * and integrity across multiple model operations.
